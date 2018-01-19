0b7aafa91b242fe6a1f341a1bac82871c60dff8c
Patrick Cardoso <pbloemer59@gmail.com>
2018-01-19 06:52:56 -0200

    Dei um tapa no negócio

---
+++ Harvin/Harvin/Migrations/201801190714354_CriarBanco2.cs
@@ -0,0 +1,16 @@
+namespace Harvin.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CriarBanco2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
