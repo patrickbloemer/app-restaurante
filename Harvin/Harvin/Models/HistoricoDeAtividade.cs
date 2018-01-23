4ead53d7833753a480078f6aa1486bab92e23cc6
Patrick Cardoso <pbloemer59@gmail.com>
2018-01-23 09:58:59 -0200

    Adicionada Classe de Histórico de Ações. Será utilizada para marcar toda e qualquer ação dentro do sistema em referencia a CRUD e Login

---
+++ Harvin/Harvin/Models/HistoricoDeAtividade.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace Harvin.Models {
+    [Table("HistoricoDeAtividades")]
+    public class HistoricoDeAtividade {
+        [Key]
+        public int id { get; set; }
+        public Funcionario funcionario { get; set; }
+        public DateTime dataHorario { get; set; }
+        public string acao { get; set; }
+    }
+}
