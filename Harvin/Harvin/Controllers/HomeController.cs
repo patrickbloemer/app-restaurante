195ed39b57a2f96337cd0d1d406056f681f62e09
Patrick Cardoso <pbloemer59@gmail.com>
2018-01-23 22:27:57 -0200

    Finalizei o site e adicionei página de imagens

--- Harvin/Harvin/Controllers/HomeController.cs
+++ Harvin/Harvin/Controllers/HomeController.cs
@@ -6,12 +6,17 @@ using System.Web.Mvc;
 
 namespace Harvin.Controllers
 {
-    public class HomeController : Controller
-    {
+    public class HomeController : Controller {
         // GET: Home
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
+            return View();
+        }
+
+        // GET: Pizzaria
+        public ActionResult Pizzaria() {
             return View();
         }
     }
+
+
 }
