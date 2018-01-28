d08ef98e02daa5b51c9d73d2b9b545641756725c
Lucas Leonardo <leus12pa@hotmail.com>
2018-01-28 04:54:24 -0200

    CATEGORIA - Mostrar a quantidade de produtos cadastrados na categoria através do card da categoria.

--- Harvin/Harvin/Controllers/CategoriasController.cs
+++ Harvin/Harvin/Controllers/CategoriasController.cs
@@ -198,7 +198,7 @@ namespace Harvin.Controllers
             return View(categoria);
         }
 
-        // POST: Cargos/Ativar
+        // POST: Categoria/Ativar
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
